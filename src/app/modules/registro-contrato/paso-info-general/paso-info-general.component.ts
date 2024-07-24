95c4b0d4004aaf956335645b881226c5847a0e35
LauwuRi <lain.2ri@gmail.com>
2024-07-24 17:30:51 -0500

    feat: Creación de más funciones para integracion udistrital/argo_documentacion#81

--- src/app/modules/registro-contrato/paso-info-general/paso-info-general.component.ts
+++ src/app/modules/registro-contrato/paso-info-general/paso-info-general.component.ts
@@ -37,7 +37,7 @@ export class PasoInfoGeneralComponent {
   unidad_ejecucion: any[] = [];
 
   // orden-contrato
-  perfinl_contratista: any[] = [];
+  perfil_contratista: any[] = [];
   fecha_suscripcion: any[] = [];
   aplica_poliza: any[] = [];
   // convenio
@@ -47,8 +47,14 @@ export class PasoInfoGeneralComponent {
 
   ngOnInit():void{
     this.CargarCompromisos();
-
+    //this.CargarContratos(id_compromiso);
+    this.CargarModalidadSeleccion();
+    this.CargarRegimenContratacion();
+    this.CargarProcedimiento();
+    this.CargarUnidadEjecucion();
   }
+
+  //Generales
 
   CargarCompromisos(){
     this.parametrosService.get('parametro?query=TipoParametroId:' + environment.TIPO_COMPROMISO_ID + '&limit=0').subscribe((Response: any) => {
@@ -58,11 +64,64 @@ export class PasoInfoGeneralComponent {
     })
   }
 
-  CargarContratos(){
-    this.parametrosService.get('parametro?query=ParametroPadreId:' + /*id compromiso*/ + '&TipoParametroId:'+ environment.TIPO_CONTRATO_ID +'&limit=0').subscribe((Response: any) => {
+  CargarModalidadSeleccion(){
+    this.parametrosService.get('parametro?query=TipoParametroId:' + environment.MODALIDAD_SELECCION_ID + '&limit=0').subscribe((Response: any) => {
       if(Response.Status == "200"){
-        this.compromisos = Response.Data;
+        this.modalidad_seleccion = Response.Data;
       }
     })
   }
+
+  CargarRegimenContratacion(){
+    this.parametrosService.get('parametro?query=TipoParametroId:' + environment.REGIMEN_CONTRATACION_ID + '&limit=0').subscribe((Response: any) => {
+      if(Response.Status == "200"){
+        this.regimen_contratacion = Response.Data;
+      }
+    })
+  }
+
+  CargarProcedimiento(){
+    this.parametrosService.get('parametro?query=TipoParametroId:' + environment.PROCEDIMIENTO_ID + '&limit=0').subscribe((Response: any) => {
+      if(Response.Status == "200"){
+        this.procedimiento = Response.Data;
+      }
+    })
+  }
+
+  CargarUnidadEjecucion(){
+    this.parametrosService.get('parametro?query=TipoParametroId:' + /*environment.UNIDAD_EJECUCION_ID*/ + '&limit=0').subscribe((Response: any) => {
+      if(Response.Status == "200"){
+        this.procedimiento = Response.Data;
+      }
+    })
+  }
+
+  //Especificos
+
+  CargarContratos(id_compromiso: undefined){
+    this.parametrosService.get('parametro?query=ParametroPadreId:' + id_compromiso + '&TipoParametroId:'+ environment.TIPO_CONTRATO_ID +'&limit=0').subscribe((Response: any) => {
+      if(Response.Status == "200"){
+        this.contratos = Response.Data;
+      }
+    })
+  }
+
+  CargarTipologiaEspecifica(id_contrato: undefined){
+    this.parametrosService.get('parametro?query=ParametroPadreId:' + id_contrato + '&TipoParametroId:'+ environment.TIPOLOGIA_ESPECIFICA_ID +'&limit=0').subscribe((Response: any) => {
+      if(Response.Status == "200"){
+        this.tipologia_especifica = Response.Data;
+      }
+    })
+  }
+
+  CargarPerfilContratista(id_contrato: undefined){
+    if(id_contrato == '6546'){
+      this.parametrosService.get('parametro?query=TipoParametroId:' + environment.PERFIL_CONTRATISTA_ID + '&ParametroPadreId:'+ id_contrato +'&limit=0').subscribe((Response: any) => {
+        if(Response.Status == "200"){
+          this.perfil_contratista = Response.Data;
+        }
+      })
+    }
+  }
+
 }
