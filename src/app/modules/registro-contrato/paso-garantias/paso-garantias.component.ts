80c5eb53b4b3c26da59d155d0f120ac4e03e9cea
LauwuRi <lain.2ri@gmail.com>
2024-08-10 11:41:02 -0500

    feat: Cambio de entrada suficiencia y validaciones udistrital/argo_documentacion#99

--- src/app/modules/registro-contrato/paso-garantias/paso-garantias.component.ts
+++ src/app/modules/registro-contrato/paso-garantias/paso-garantias.component.ts
@@ -2,12 +2,8 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ParametrosService } from 'src/app/services/parametros.service';
 import { environment } from 'src/environments/environment';
-
-interface Fila {
-  amparo: string;
-  suficiencia: string;
-  descripcion: string;
-}
+import { BehaviorSubject, Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-paso-garantias',
@@ -16,16 +12,15 @@ interface Fila {
 })
 export class PasoGarantiasComponent implements OnInit {
   form: FormGroup;
-
   amparos: any[] = [];
-
   displayedColumns = ['id', 'amparo', 'suficiencia', 'descripcion', 'acciones'];
-  dataSource: Fila[] = [];
+  dataSource: MatTableDataSource<FormGroup>;
 
   constructor(private _formBuilder: FormBuilder, private parametrosService: ParametrosService, private cdRef: ChangeDetectorRef) {
     this.form = this._formBuilder.group({
       filas: this._formBuilder.array([])
     });
+    this.dataSource = new MatTableDataSource<FormGroup>([]);
   }
 
   ngOnInit() {
@@ -40,13 +35,16 @@ export class PasoGarantiasComponent implements OnInit {
   crearFilaFormGroup(): FormGroup {
     return this._formBuilder.group({
       amparo: ['', Validators.required],
-      suficiencia: ['', Validators.required],
+      suficienciaPorcentaje: ['', Validators.required],
+      suficienciaSalarios: [{ value: '', disabled: true }],
       descripcion: ['', Validators.required]
     });
   }
 
   agregarFila() {
-    this.filasFormArray.push(this.crearFilaFormGroup());
+    const nuevaFila = this.crearFilaFormGroup();
+    this.filasFormArray.push(nuevaFila);
+    this.configurarAmparoListener(nuevaFila);
     this.actualizarDataSource();
   }
 
@@ -56,7 +54,36 @@ export class PasoGarantiasComponent implements OnInit {
   }
 
   actualizarDataSource() {
-    this.dataSource = this.filasFormArray.controls.map(control => control.value);
+    this.dataSource.data = this.filasFormArray.controls as FormGroup[];
+    this.cdRef.detectChanges();
+  }
+
+  configurarAmparoListener(filaFormGroup: FormGroup) {
+    filaFormGroup.get('amparo')?.valueChanges.subscribe((id_amparo) => {
+      if (id_amparo) {
+        const idAmparoStr = id_amparo.toString();
+        const tipoAmparoIdStr = environment.AMPARO_CREC_ID.toString();
+
+        const suficienciaPorcentajeControl = filaFormGroup.get('suficienciaPorcentaje');
+        const suficienciaSalariosControl = filaFormGroup.get('suficienciaSalarios');
+
+        if (idAmparoStr === tipoAmparoIdStr) {
+          suficienciaPorcentajeControl?.disable();
+          suficienciaPorcentajeControl?.setValue('');
+          suficienciaSalariosControl?.enable();
+          suficienciaSalariosControl?.setValidators(Validators.required);
+        } else {
+          suficienciaPorcentajeControl?.enable();
+          suficienciaPorcentajeControl?.setValidators(Validators.required);
+          suficienciaSalariosControl?.disable();
+          suficienciaSalariosControl?.setValue('');
+        }
+
+        suficienciaPorcentajeControl?.updateValueAndValidity();
+        suficienciaSalariosControl?.updateValueAndValidity();
+        this.cdRef.detectChanges();
+      }
+    });
   }
 
   CargarAmparos() {
@@ -65,6 +92,55 @@ export class PasoGarantiasComponent implements OnInit {
         this.amparos = Response.Data;
       }
     })
+  }
+
+  onlyNumbersFrom1To100(event: KeyboardEvent, currentValue: string) {
+    const allowedKeys = [
+      'Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete'
+    ];
+
+    if (allowedKeys.includes(event.key)) {
+      return;
+    }
+
+    const pattern = /^[0-9]$/;
+
+    if (!pattern.test(event.key)) {
+      event.preventDefault();
+      return;
+    }
+
+    let newValue = currentValue;
+    const cursorPosition = (event.target as HTMLInputElement).selectionStart;
+    if (cursorPosition !== null) {
+      newValue = currentValue.slice(0, cursorPosition) + event.key + currentValue.slice(cursorPosition);
+    } else {
+      newValue += event.key;
+    }
+
+    const numValue = parseInt(newValue, 10);
+    if (numValue < 1 || numValue > 100 || newValue.length > 3) {
+      event.preventDefault();
+    }
+  }
+
+  onlyPositiveIntegers(event: KeyboardEvent, currentValue: string) {
+    const allowedKeys = [
+      'Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete'
+    ];
+    const pattern = /^[0-9]$/;
+
+    if (!allowedKeys.includes(event.key) && !pattern.test(event.key)) {
+      event.preventDefault();
+      return;
+    }
+
+    const newValue = currentValue + event.key;
+    const numValue = parseInt(newValue, 10);
+
+    if (numValue === 0) {
+      event.preventDefault();
+    }
   }
 
 }
