810ed87b0202080d15fdbc128672646b161334b6
LauwuRi <lain.2ri@gmail.com>
2024-08-15 11:27:34 -0500

    feat: Implementacion vigencia y tipo persona udistrital/argo_documentacion#103

--- src/app/modules/consulta-contrato/consulta-contrato.component.ts
+++ src/app/modules/consulta-contrato/consulta-contrato.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { ParametrosService } from 'src/app/services/parametros.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-consulta-contrato',
@@ -8,7 +10,7 @@ import { FormBuilder } from '@angular/forms';
 })
 export class ConsultaContratoComponent {
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder, private parametrosService: ParametrosService) { }
 
   form = this._formBuilder.group({
     unidadEjecutora: [''],
@@ -22,5 +24,29 @@ export class ConsultaContratoComponent {
     fechaDesde: [''],
     fechaHasta: [''],
   });
-  
+
+  vigencia: any[] = [];
+  tipoPersona: any[] = [];
+
+  ngOnInit(): void {
+    this.CargarVigencia();
+    this.CargarTipoPersona();
+  }
+
+  CargarVigencia() {
+    this.parametrosService.get('parametro?query=TipoParametroId:' + environment.VIGENCIA_ID + '&limit=0').subscribe((Response: any) => {
+      if (Response.Status == "200") {
+        this.vigencia = Response.Data;
+      }
+    })
+  }
+
+  CargarTipoPersona() {
+    this.parametrosService.get('parametro?query=TipoParametroId:' + environment.TIPO_PERSONA_ID + '&limit=0').subscribe((Response: any) => {
+      if (Response.Status == "200") {
+        this.tipoPersona = Response.Data;
+      }
+    })
+  }
+
 }
