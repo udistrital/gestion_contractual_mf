06f51649c1478621fb8e31861579865bc6369986
LauwuRi <lain.2ri@gmail.com>
2024-08-29 22:01:41 -0500

    feat: Integración Ubicaciones en Lugar ejecución udistrital/argo_documentacion#116

--- src/app/modules/registro-contrato/paso-supervisores/paso-supervisores.component.ts
+++ src/app/modules/registro-contrato/paso-supervisores/paso-supervisores.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UbicacionService } from 'src/app/services/ubicacion.service';
 
@@ -13,6 +13,7 @@ export class PasoSupervisoresComponent {
   constructor(
     private _formBuilder: FormBuilder,
     private ubicacionService: UbicacionService,
+    private cdRef: ChangeDetectorRef
   ) { }
 
   form = this._formBuilder.group({
@@ -44,12 +45,40 @@ export class PasoSupervisoresComponent {
   ngOnInit(): void {
     this.CargarPais();
 
+    this.form.get('pais')?.valueChanges.subscribe((id_pais) => {
+      if (id_pais) {
+        this.CargarDepartamento(id_pais);
+      }
+    });
+
+    this.form.get('departamento')?.valueChanges.subscribe((id_departamento) => {
+      if (id_departamento) {
+        this.CargarCiudad(id_departamento);
+      }
+    });
+
   }
 
   CargarPais() {
     this.ubicacionService.get('lugar?query=TipoLugarId:1&limit=0').subscribe((Response: any) => {
       if (Response.length != 0) {
         this.pais = Response;
+      }
+    })
+  }
+
+  CargarDepartamento(id_pais: string) {
+    this.ubicacionService.get('relacion_lugares?query=LugarPadreId:' + id_pais + '&limit=0').subscribe((Response: any) => {
+      if (Response.length != 0) {
+        this.departamento = Response;
+      }
+    })
+  }
+
+  CargarCiudad(id_departamento: string) {
+    this.ubicacionService.get('relacion_lugares?query=LugarPadreId:' + id_departamento + '&limit=0').subscribe((Response: any) => {
+      if (Response.length != 0) {
+        this.municipioCiudad = Response;
       }
     })
   }
