d2902b95f15d3d402056e6de6558e0e295838525
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-09-18 19:43:52 -0500

    feat: Implementación del editor quill udistrital/argo_documentacion/issues/139

--- src/app/components/editor-enriquecido/editor-enriquecido..component.ts
+++ src/app/components/editor-enriquecido/editor-enriquecido..component.ts
@@ -1,5 +1,5 @@
-import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {Component, OnInit} from "@angular/core";
+import {ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
+import {Component, forwardRef, Input, OnInit} from "@angular/core";
 import {QuillEditorComponent} from "ngx-quill";
 import Italic from "quill/formats/italic";
 
@@ -11,20 +11,28 @@ import Italic from "quill/formats/italic";
     ReactiveFormsModule
   ],
   templateUrl: './editor-enriquecido.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => EditorEnriquecidoComponent),
+      multi: true
+    }
+  ],
   styles: [`
     quill-editor {
       height: 200px;
+      width: 100%;
       margin-bottom: 20px;
-    }
-    .preview {
-      border: 1px solid #ccc;
-      padding: 10px;
-      background-color: #f9f9f9;
     }
   `]
 })
-export class EditorEnriquecidoComponent implements OnInit {
+export class EditorEnriquecidoComponent implements OnInit, ControlValueAccessor {
+
+  @Input() placeholder: string = 'Escribe aquí...';
+
   editorForm: FormGroup;
+  onChanges: any = () => {};
+  onTouch: any = () => {};
 
   constructor(private fb: FormBuilder) {
     this.editorForm = this.fb.group({
@@ -33,6 +41,32 @@ export class EditorEnriquecidoComponent implements OnInit {
   }
 
   ngOnInit() {
+    const editorContent = this.editorForm.get('editorContent');
+    if (editorContent) {
+      editorContent.valueChanges.subscribe((value) => {
+        this.onChanges(value);
+        this.onTouch();
+      });
+    }
+  }
+
+
+  registerOnChange(fn: any): void {
+    this.onChanges = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(obj: any): void {
+    if (obj !== undefined) {
+      this.editorForm.patchValue({editorContent: obj}, {emitEvent: false});
+    }
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.editorForm.disable() : this.editorForm.enable();
   }
 
   protected readonly Italic = Italic;
