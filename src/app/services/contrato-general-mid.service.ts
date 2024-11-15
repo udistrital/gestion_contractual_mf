14095a81d50da30b278d72a3015168a84f01a4f4
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-11-15 14:31:05 -0500

    feat: Implementación mf
    https://github.com/udistrital/argo_documentacion/issues/194

--- src/app/services/contrato-general-mid.service.ts
+++ src/app/services/contrato-general-mid.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {RequestManager} from "../managers/requestManager";
 import {Observable} from "rxjs";
+import {ApiResponse, DependenciaContratoMidResponse, SedeContratoMidResponse} from "../types/types";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,35 @@ export class ContratoGeneralMidService {
   constructor(private requestManager: RequestManager) {
     this.requestManager.setPath('GESTION_CONTRACTUAL_MID_SERVICE');
   }
+
   get(id_contrato: any): Observable<any> {
     this.requestManager.setPath('GESTION_CONTRACTUAL_MID_SERVICE');
     return this.requestManager.get(`contratos-generales/${id_contrato}`);
+  }
+
+  getSedes(): Observable<SedeContratoMidResponse[]> {
+    this.requestManager.setPath('GESTION_CONTRACTUAL_MID_SERVICE');
+    return this.requestManager.get('espacios-fisicos/sedes')
+      .pipe(
+        map((response: any) => {
+          if (response && response.Data) {
+            return response.Data;
+          }
+          return [];
+        })
+      );
+  }
+
+  getDependenciasBySede(sedeId: number): Observable<DependenciaContratoMidResponse[]> {
+    this.requestManager.setPath('GESTION_CONTRACTUAL_MID_SERVICE');
+    return this.requestManager.get(`espacios-fisicos/dependencias-sede/${sedeId}`)
+      .pipe(
+        map((response: any) => {
+          if (response && response.Data) {
+            return response.Data;
+          }
+          return [];
+        })
+      );
   }
 }
