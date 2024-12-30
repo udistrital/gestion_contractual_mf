4a2bfee1b9d9e937bc92494c380f8aabdf9cb371
unknown <collegek450@gmail.com>
2024-12-30 16:32:57 -0500

    feat: udistrital/argo_documentacion#287 Cambio de interfaz en vista de consulta de contrato

--- src/app/modules/consulta-contrato/consulta-contrato.module.ts
+++ src/app/modules/consulta-contrato/consulta-contrato.module.ts
@@ -1,62 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsultaContratoComponent } from './consulta-contrato.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DetalleContratoComponent } from './detalle-contrato/detalle-contrato.component';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
-import {
-  MatProgressSpinner,
-  MatProgressSpinnerModule,
-} from '@angular/material/progress-spinner';
-import { MatTable, MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSortModule } from '@angular/material/sort';
 import { ModalObservacionesComponent } from './modal-observaciones/modal-observaciones.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ConsultaContratoComponent, ModalObservacionesComponent],
-  imports: [
-    CommonModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    FormsModule,
-    ReactiveFormsModule,
-    DetalleContratoComponent,
-    MatPaginator,
-    MatIcon,
-    MatProgressSpinner,
-    MatTable,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatInputModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatDividerModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    MatDialogActions,
-  ],
+  imports: [CommonModule, SharedModule, DetalleContratoComponent],
 })
 export class ConsultaContratoModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PlantillaTarjetaContenedoraComponent } from './templates/plantilla-tarjeta-contenedora/plantilla-tarjeta-contenedora.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './modules/material.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [PlantillaTarjetaContenedoraComponent],
-  imports: [CommonModule, MatCardModule, MatIconModule],
-  exports: [PlantillaTarjetaContenedoraComponent],
+  imports: [MaterialModule, CommonModule],
+  exports: [
+    PlantillaTarjetaContenedoraComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class SharedModule {}
